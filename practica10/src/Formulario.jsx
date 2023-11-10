47ab85c5587734506339af97d98f217ac02cc67e
OmarVX <omarlomo22@gmail.com>
2023-11-09 20:30:34 -0600

    practica10

---
+++ practica10/src/Formulario.jsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from "react";
+import { TextField, Button, Box } from "@mui/material";
+import axios from "axios"
+
+function Formulario(props) {
+    const [Cargando, setCargando] = useState(false)
+    const [datosFormulario, setDatosFormulario] = useState({nombre:'', password:''})
+
+    const hacerPeticion = async () => {
+        try{
+            const response = await axios.get('http://localhost:4567/tipo-usuario')
+            console.log("hacerPeticion", response)
+            return response.data
+        }catch (error){
+            throw error
+        }
+    }
+
+    const cambiosFormulario = (evento) => {
+        //console.log(evento.target)
+        const {name, value} = evento.target
+        setDatosFormulario( { ...datosFormulario, [name] : value })
+    }
+
+
+    const procesarFormulario = async (evento) => {
+        evento.preventDefault()
+        console.log("datos recuperados del form:", datosFormulario)
+        setCargando(true)
+        try {
+            const response = await hacerPeticion()
+            console.log("procesarFormulario", response)
+            setCargando(false)
+            if (datosFormulario.nombre === response.tipousuario) {
+                console.log("Usuario correcto")
+            } else {
+                console.log("Usuario incorrecto")
+            }
+        } catch (error) {
+            console.log("error", error)
+            setCargando(false)
+        }
+    }
+
+    return (
+        <>
+            <h1>Inicio de sesión</h1>
+            <form onSubmit={procesarFormulario} >
+                <Box m={5}>
+                <TextField label="Nombre:" variant="outlined" fullWidth onChange={cambiosFormulario} name="nombre" value={datosFormulario.nombre}></TextField>
+                    </Box>
+                <Box m={5}>
+                    <TextField label="Contraseña:" variant="outlined" type="password" fullWidth onChange={cambiosFormulario} name="password" value={datosFormulario.password}></TextField>
+                </Box>
+                <Box m={5}>
+                    <Button variant="contained" type= "submit" color="primary" fullWidth disabled={Cargando}>Iniciar sesión</Button>
+                </Box>
+
+            </form>
+        </>
+    )
+    
+}
+
+export default Formulario
